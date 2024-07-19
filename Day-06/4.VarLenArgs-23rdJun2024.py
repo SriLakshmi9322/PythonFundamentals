0cfffbe4d05cc0b5fea644614ef078df09ef001c
SriLaksmi <104779537+SriLakshmi9322@users.noreply.github.com>
2024-07-19 19:06:18 +0530

    Arbitrary Arguements

--- Day-06/4.VarLenArgs-23rdJun2024.py
+++ Day-06/4.VarLenArgs-23rdJun2024.py
@@ -25,3 +25,38 @@ display_arguements(10, 20.3, 'Bhavani')
 # Variable Arguement : 10
 # Variable Arguement : 20.3
 # Variable Arguement : Bhavani
+
+
+# Arbitrary Keyword Arguements :
+"""
+In Python Arbitrary Keyword Arguments, *args, and **kwargs can pass a variable
+number of arguments to a function using special symbols. There are two special
+symbols:
+1. *args in Python (Non-Keyword Arguements)
+2. **kwargs in Python (Keyword Arguements)
+"""
+
+
+# Example-1 : Variable length non-keyword arguements.
+def myFun(*args):
+    for arg in args:
+        print(arg, end=" ")
+
+
+myFun("Hello,", "Welcome", "to", "Programming.")
+
+# Output : Hello, Welcome to Programming.
+
+
+# Example-2 : Variable length Keyword Arguements.
+def myFunc(**kwargs):
+    for key, value in kwargs.items():
+        print("%s == %s" % (key, value))
+
+
+myFunc(fname="SriLakshmi", lname="Tiramsetti", age=21)
+
+# Output :
+# fname == SriLakshmi
+# lname == Tiramsetti
+# age == 21
